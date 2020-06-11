970691d0c56ebac97eaec83fd7d42a7c631eb043
stelzer-monar <stelzer.monar@ifood.com.br>
2020-06-10 23:11:16 -0300

    Fix path of detector log

--- server/runServer.py
+++ server/runServer.py
@@ -4,6 +4,7 @@ import sys
 import subprocess
 
 imagesPath = os.getcwd() + "/img"
+detectorPath = os.getcwd() + "/detector.log"
 # Start api process
 p1 = subprocess.Popen(['gunicorn', '-k', 'geventwebsocket.gunicorn.workers.GeventWebSocketWorker', '-w', '4', '-b', '0.0.0.0', 'main:app'])
 # Start computation process
@@ -11,7 +12,7 @@ p2 = subprocess.Popen(['python3', 'startDetection.py', imagesPath])
 
 time.sleep(2)
 # Start collecting process
-p3 = subprocess.Popen(['python3', 'saveResults.py', imagesPath])
+p3 = subprocess.Popen(['python3', 'saveResults.py', detectorPath])
 
 while True:
     time.sleep(60)
